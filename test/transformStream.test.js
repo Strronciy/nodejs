6c8989117662bad29f15d797fd8a5807ca5f5c97
Strronciy <str0nciy@ya.ru>
2021-11-21 23:52:28 +0300

    fix: change tests

--- test/transformStream.test.js
+++ test/transformStream.test.js
@@ -1,22 +1,22 @@
 import TransformStream from "../ciphering-cli/transformStream";
 
-test("Should cipher by caesar 'A' to 'B'", () => {
+test("Should cipher by caesar 'Hello!' to 'Ifmmp!'", () => {
     const transform = new TransformStream();
     transform.cipher = "C";
     transform.shift = 1;
     transform.encoding = 1;
-    expect(transform.transformByShift("A")).toBe("B");
+    expect(transform.transformByShift("Hello!")).toBe("Ifmmp!");
 });
 
-test("Should cipher by atbash 'A' to 'Z'", () => {
+test("Should cipher by atbash 'Hello!' to 'Svool!'", () => {
     const transform = new TransformStream();
-    expect(transform.transformAtbash("A")).toBe("Z");
+    expect(transform.transformAtbash("Hello!")).toBe("Svool!");
 });
 
-test("Should cipher by ROT8 'A' to 'I'", () => {
+test("Should cipher by ROT8 'Hello!' to 'Pmttw!'", () => {
     const transform = new TransformStream();
     transform.cipher = "R";
     transform.shift = 8;
     transform.encoding = 1;
-    expect(transform.transformByShift("A")).toBe("I");
+    expect(transform.transformByShift("Hello!")).toBe("Pmttw!");
 });
