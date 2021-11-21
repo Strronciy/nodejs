836476d1e786510cbaab196c558b5378d775aa8a
Strronciy <str0nciy@ya.ru>
2021-11-21 23:22:55 +0300

    feat: add createTransformStreams test

--- ciphering-cli/createTransformStreams.js
+++ ciphering-cli/createTransformStreams.js
@@ -7,8 +7,12 @@ export const createTransformStreams = (config) => {
         const cipher = streamConfig[0];
         const encoding = Number(streamConfig[1]);
         let shift;
-        if (cipher === "C") shift = 1;
-        if (cipher === "R") shift = 8;
+        switch (cipher) {
+            case "C":
+                shift = 1;
+            case "R":
+                shift = 8;
+        }
         streams.push(new TransformStream(cipher, shift, encoding));
     });
     return streams;
